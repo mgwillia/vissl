8c8b69e73811571248595c7f54180dbb23e12436
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-06-28 11:59:41 -0400

    changes

--- batch_scripts/swav_finetune.sh
+++ batch_scripts/swav_finetune.sh
@@ -5,7 +5,7 @@
 #SBATCH --error=outfiles/swav_f_inet.out.%j                             # indicates a file to redirect STDERR to; %j is the jobid
 #SBATCH --time=36:00:00                                          # how long you think your job will take to complete; format=hh:mm:ss
 #SBATCH --qos=high
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:p6000:4
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=8
 
@@ -15,4 +15,4 @@ srun bash -c 'hostname; CUDA_VISIBLE_DEVICES=0,1,2,3 python ./tools/run_distribu
     config.DISTRIBUTED.NUM_PROC_PER_NODE=4 config.DISTRIBUTED.NUM_NODES=1 \
     config.DATA.TRAIN.DATA_PATHS=["/fs/vulcan-datasets/imagenet"] \
     config.MODEL.WEIGHTS_INIT.PARAMS_FILE="swav_pretrained.torch" \
-    config.CHECKPOINT.DIR="./checkpoints_swav" config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=32'
+    config.CHECKPOINT.DIR="./checkpoints_swav" config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=100'
