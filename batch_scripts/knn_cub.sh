6591d33a2a07f0817987798f7993c3a39ccf0f09
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-10-19 17:42:44 -0400

    y

--- batch_scripts/knn_cub.sh
+++ batch_scripts/knn_cub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=b_cub
-#SBATCH --output=outfiles/b_cub.out.%j
-#SBATCH --error=outfiles/b_cub.out.%j
+#SBATCH --job-name=knn_cub
+#SBATCH --output=outfiles/knn_cub.out.%j
+#SBATCH --error=outfiles/knn_cub.out.%j
 #SBATCH --time=36:00:00
 #SBATCH --account=scavenger
 #SBATCH --partition=scavenger
@@ -12,16 +12,16 @@
 
 module load cuda/10.1.243
 
-INDICES=(0 1 2 3 4 5 6)
-BACKBONES=("btwins_r50_1000" "dcv2_r50_800" "moco_r50_800" "simclr_r50_200" "simsiam_r50_100" "supervised_r50" "swav_r50_800")
-CHECKPOINTS_DIRS=("chkpts_b_cub_btwins" "chkpts_b_cub_dc" "chkpts_b_cub_moco" "chkpts_b_cub_sclr" "chkpts_b_cub_simsiam" "chkpts_b_cub_sup" "chkpts_b_cub_swav")
-STATE_DICT_KEY_NAMES=("classy_state_dict" "network" "network" "classy_state_dict" "network" "network" "classy_state_dict")
-
 if [ ! -d /scratch0/mgwillia/CUB_200_2011 ]; then
     srun bash -c "echo 'cub not found on scratch!'"
     srun bash -c "mkdir -p /scratch0/mgwillia"
-    srun bash -c "rsync -r /vulcanscratch/mgwillia/CUB_200_2011 /scratch0/mgwillia/"
+    srun bash -c "./msrsync -P -p 16 /vulcanscratch/mgwillia/CUB_200_2011 /scratch0/mgwillia/"
 fi
+
+INDICES=(0 1 2 3 4 5 6)
+BACKBONES=("btwins_r50_1000" "dcv2_r50_800" "moco_r50_800" "simclr_r50_200" "simsiam_r50_100" "supervised_r50" "swav_r50_800")
+CHECKPOINTS_DIRS=("chkpts_knn_cub_btwins" "chkpts_knn_cub_dc" "chkpts_knn_cub_moco" "chkpts_knn_cub_sclr" "chkpts_knn_cub_simsiam" "chkpts_knn_cub_sup" "chkpts_knn_cub_swav")
+STATE_DICT_KEY_NAMES=("classy_state_dict" "network" "network" "classy_state_dict" "network" "network" "classy_state_dict")
 
 for i in ${INDICES[@]}; do
     srun bash -c "hostname; python ./tools/run_distributed_engines.py \
