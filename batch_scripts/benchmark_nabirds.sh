aef556ca0629e462a4b08a07430d1ed2bc4cd1d3
Matt Gwilliam <mattgwilliamjr@gmail.com>
2021-10-25 11:28:27 -0400

    y

--- batch_scripts/benchmark_nabirds.sh
+++ batch_scripts/benchmark_nabirds.sh
@@ -4,8 +4,7 @@
 #SBATCH --output=outfiles/b_nabirds.out.%j
 #SBATCH --error=outfiles/b_nabirds.out.%j
 #SBATCH --time=36:00:00
-#SBATCH --account=scavenger
-#SBATCH --partition=scavenger
+#SBATCH --qos=high
 #SBATCH --gres=gpu:gtx1080ti:4
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=16
@@ -17,10 +16,10 @@ BACKBONES=("btwins_r50_1000" "dcv2_r50_800" "moco_r50_800" "simclr_r50_200" "sim
 CHECKPOINTS_DIRS=("chkpts_b_nabirds_btwins" "chkpts_b_nabirds_dc" "chkpts_b_nabirds_moco" "chkpts_b_nabirds_sclr" "chkpts_b_nabirds_simsiam" "chkpts_b_nabirds_sup" "chkpts_b_nabirds_swav")
 STATE_DICT_KEY_NAMES=("classy_state_dict" "network" "network" "classy_state_dict" "network" "network" "classy_state_dict")
 
-if [ ! -d /scratch0/mgwillia/NABirds ]; then
+if [ ! -d /scratch0/mgwillia/nabirds ]; then
     srun bash -c "echo 'nabirds not found on scratch!'"
     srun bash -c "mkdir -p /scratch0/mgwillia"
-    srun bash -c "./msrsync -P -p 16 /vulcanscratch/mgwillia/NABirds /scratch0/mgwillia/"
+    srun bash -c "./msrsync -P -p 16 /vulcanscratch/mgwillia/nabirds /scratch0/mgwillia/"
 fi
 
 for i in ${INDICES[@]}; do
@@ -30,8 +29,8 @@ for i in ${INDICES[@]}; do
                     config.MODEL.WEIGHTS_INIT.PARAMS_FILE=/vulcanscratch/mgwillia/unsupervised-classification/backbones/${BACKBONES[$i]}.torch \
                     config.MODEL.WEIGHTS_INIT.STATE_DICT_KEY_NAME=${STATE_DICT_KEY_NAMES[$i]} \
                     config.DISTRIBUTED.NUM_PROC_PER_NODE=4 config.DISTRIBUTED.NUM_NODES=1 \
-                    config.DATA.TRAIN.DATA_PATHS=['/scratch0/mgwillia/NABirds/train'] \
-                    config.DATA.TEST.DATA_PATHS=['/scratch0/mgwillia/NABirds/val'] \
+                    config.DATA.TRAIN.DATA_PATHS=['/scratch0/mgwillia/nabirds/train'] \
+                    config.DATA.TEST.DATA_PATHS=['/scratch0/mgwillia/nabirds/val'] \
                     config.CHECKPOINT.DIR=/vulcanscratch/mgwillia/vissl/${CHECKPOINTS_DIRS[$i]} \
                     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=64"
 done
